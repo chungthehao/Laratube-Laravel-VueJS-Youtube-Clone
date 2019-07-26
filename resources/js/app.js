6ea93a78baff2991dd0607ff69581bc66710a4ee
Henry Chung <chungthehao@gmail.com>
2019-07-26 17:39:42 +0700

    Reusable votes component

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ window.Vue = require('vue');
 Vue.config.ignoredElements = ['video-js'];
 
 Vue.component('subscribe-button', require('./components/subscribe-button.vue').default);
+Vue.component('votes', require('./components/votes.vue').default);
 require('./components/channel-uploads');
 
 const app = new Vue({
